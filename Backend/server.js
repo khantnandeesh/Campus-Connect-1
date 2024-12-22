45e4b514535d783fd7fcb5bdb68042c8353ae819
harshit-dev-ui <65815998+harshit-dev-ui@users.noreply.github.com>
2024-12-23 00:51:20 +0530

    minor changes in directory structure

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import collegeRoutes from './routes/collegeRoutes.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import collegeRoutes from "./routes/collegeRoutes.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -19,8 +22,8 @@ app.use(
 
 connectDB();
 
-app.use('/auth', authRoutes);
-app.use('/college', collegeRoutes);
+app.use("/auth", authRoutes);
+app.use("/college", collegeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />    
-        <Route path="/signup" element={<Signup />} /> 
-        <Route path="/login" element={<Login />} />   
-        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
