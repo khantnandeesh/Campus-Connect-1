9525530bb23847418b5e803a82edf7ab1f7637f2
khantnandeesh <nandeeshkhant3205@gmail.com>
2025-03-30 00:19:16 +0530

    cors-all

--- Backend/server.js
+++ Backend/server.js
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 // Configure Socket.IO with CORS
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // Add this line
   },
